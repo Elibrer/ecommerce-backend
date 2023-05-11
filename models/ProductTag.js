89d34ea2d52df491823e86c628be9ad961523a0b
Elibrer <eli.brer@gmail.com>
2023-05-11 16:36:02 +1000

    Filled out model data, started work on category routes.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    product_tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'product',
+            key: 'product_id',
+        },
+    },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'tag',
+            key: 'tag_id',
+        },
+    },
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+  const catagoryData = await Category.findAll({
+      include: [{ model: Product }],
+  })
+  res.status(200).json(catagoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryById = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    })
+    res.status(200).json(categoryById)
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+
+  }
 });
 
 router.put('/:id', (req, res) => {
